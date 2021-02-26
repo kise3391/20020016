4afdff788e3eed5fad4329501e14a0c872835a22
kise3391 <78575707+kise3391@users.noreply.github.com>
2021-02-26 09:22:47 +0700

    14

---
+++ 13.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin >> x >> y;
+    cout << sqrt(x * x + y * y);
+    return 0;
+}
---
+++ 14.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cin >> k; // Bob xem thẻ
+    if (k < 50); // Bob chọn thẻ còn lại
+    else; // Bob chọn thẻ k
+    return 0;
+}
